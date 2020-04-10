97cd9b61a2baf138f614515f37db3d6a5e9b281f
ithewei <ithewei@163.com>
2020-04-11 00:03:22 +0800

    GL_NEAREST => GL_LINEAR

--- src/qt/HGLWidget.cpp
+++ src/qt/HGLWidget.cpp
@@ -188,8 +188,8 @@ void HGLWidget::initYUV() {
     glGenTextures(3, tex_yuv);
     for (int i = 0; i < 3; ++i) {
         glBindTexture(GL_TEXTURE_2D, tex_yuv[i]);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     }
